f3126f1d6deeb903b8e6545b983932d59c3f5e00
Tuan Vo <tuan.vo@sotatek.com>
2020-12-11 11:18:16 +0700

    Updates

---
+++ precache-manifest.65bc3de3b481f6980867aa224f4422cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "84e60708ffa0106208ce",
+    "url": "/movies-series-info-redux-github-pages/static/css/main.10ed4d32.chunk.css"
+  },
+  {
+    "revision": "84e60708ffa0106208ce",
+    "url": "/movies-series-info-redux-github-pages/static/js/main.84e60708.chunk.js"
+  },
+  {
+    "revision": "87f7407a67ef306dbf94",
+    "url": "/movies-series-info-redux-github-pages/static/js/1.87f7407a.chunk.js"
+  },
+  {
+    "revision": "141027c57fca67b23a87",
+    "url": "/movies-series-info-redux-github-pages/static/js/runtime~main.141027c5.js"
+  },
+  {
+    "revision": "6847984ba4801af5b3c58ab4370e326d",
+    "url": "/movies-series-info-redux-github-pages/index.html"
+  }
+];
